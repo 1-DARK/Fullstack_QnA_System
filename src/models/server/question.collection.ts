99a240da078122fe0334a94e6773e9dbccdeab7f
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-07-12 13:22:14 +0530

    initial commit

---
+++ src/models/server/question.collection.ts
@@ -0,0 +1,29 @@
+import { IndexType, Permission } from 'node-appwrite'
+import { db, questionCollection } from '../name'
+import { databases } from './config'
+
+export default async function createQuestionCollection() {
+    // create collection
+    await databases.createCollection(db, questionCollection,
+        questionCollection, [
+        Permission.read("any"),
+        Permission.read("users"),
+        Permission.create("users"),
+        Permission.update("users"),
+        Permission.delete("users"),
+    ])
+    console.log('Question collection is created')
+
+    // creating attributes and indexes
+    await Promise.all([
+        databases.createStringAttribute(db, questionCollection, "title", 100, true),
+        databases.createStringAttribute(db, questionCollection, "content", 10000, true),
+        databases.createStringAttribute(db, questionCollection, "authorid", 50, true),
+        databases.createStringAttribute(db, questionCollection, "tags", 50, true, undefined, true),
+        databases.createStringAttribute(db, questionCollection, "attachmentid", 50, false)
+    ])
+    console.log('Question Attribute created')
+
+    // create index
+
+}
