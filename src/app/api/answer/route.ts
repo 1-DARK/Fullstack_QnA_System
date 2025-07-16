a86958cb7e12c9f9c9bd4eb9fe584aa31f1b6c1e
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-07-16 13:28:27 +0530

    initial commit

--- src/app/api/answer/route.ts
+++ src/app/api/answer/route.ts
@@ -1,10 +1,23 @@
-import { databases } from "@/models/server/config";
+import { answerCollection, db } from "@/models/name";
+import { databases, users } from "@/models/server/config";
 import { NextRequest, NextResponse } from "next/server";
+import { ID } from "node-appwrite";
+import { UserPrefs } from "@/store/Auth";
 
 export async function POST(request: NextRequest) {
   try {
     const { questionId, answer, authorId } = await request.json();
-    const response = await databases.createDocument;
+    const response = await databases.createDocument(
+      db,
+      answerCollection,
+      ID.unique(),
+      { content: answer, authorId: authorId, questionId: questionId }
+    );
+    // Increase author reputation
+    const prefs = await users.getPrefs<UserPrefs>(authorId);
+    await users.updatePrefs(authorId, {
+      reputation: Number(prefs.reputation) + 1,
+    });
   } catch (error: any) {
     return NextResponse.json(
       {
