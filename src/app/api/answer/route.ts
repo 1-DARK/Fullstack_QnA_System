9e5f493f53977dc951d8fbd741e66fc4ba87195d
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-07-16 13:15:56 +0530

    initial commit

--- src/app/api/answer/route.ts
+++ src/app/api/answer/route.ts
@@ -1,1 +1,18 @@
+import { databases } from "@/models/server/config";
 import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { questionId, answer, authorId } = await request.json();
+    const response = await databases.createDocument;
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        error: error?.message || "Error creating answer",
+      },
+      {
+        status: error?.status || error?.code || 500,
+      }
+    );
+  }
+}
