013dfb91e10893ebfe875ce6dc402259c2e1548a
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-07-16 13:00:56 +0530

    initial commit

--- src/app/api/answer/route.ts
+++ src/app/api/answer/route.ts
@@ -1,11 +1,1 @@
 import { NextRequest, NextResponse } from "next/server";
-
-export async function POST(request: NextRequest) {
-    try {
-
-    } catch (error: any) {
-        return NextResponse.json({
-            error: error?.message || "Error creating answer"
-        })
-    }
-}
