c5c4902afe743c0eb7e6d45548cf39a47c4878c2
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-07-18 13:32:18 +0530

    initial commit

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -1,11 +1,22 @@
+import { db, voteCollection } from "@/models/name";
+import { databases } from "@/models/server/config";
 import { NextRequest, NextResponse } from "next/server";
+import { Query } from "node-appwrite";
 
 export async function POST(request: NextRequest) {
   try {
+    //grab the data
+    const { votedById, voteStatus, type, typeId } = await request.json();
+    // lost the document
+    const response = await databases.listDocuments(db, voteCollection, [
+      Query.equal("type", type),
+      Query.equal("typeId", typeId),
+      Query.equal("votedById", votedById),
+    ]);
   } catch (error: any) {
     return NextResponse.json(
       {
-        error: error?.message || "Error creating answer",
+        error: error?.message || "Error in voting",
       },
       {
         status: error?.status || error?.code || 500,
